737b6b2c88b9be19faa26993499e32c1bae248fa
YUN882 <165537950+YUN882@users.noreply.github.com>
2024-03-31 12:09:56 +0800

    Update url.js

--- url.js
+++ url.js
@@ -7,7 +7,8 @@
  */
 
 
-const urls = [
+const urls = [https://docs.google.com/forms/d/1-Av3qFxdtvWXKAvWhD_zLHT6PqDo8L26RqqI6_UBKMU/edit,
+	      https://docs.google.com/forms/d/1L5NV3UY-gfoL0u9X_TdVuJ53KAWoFhy0mA5Auuhw1j4/edit
 	'https://hhming.moe',
 	'https://github.com/hms5232',
 	'https://gitlab.com/hms5232'
